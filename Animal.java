84da6397997f8d19ba82fa325e880316a82815a6
Pradsan07 <priyoas7@gmail.com>
2025-03-06 09:55:15 +0700

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,7 @@
+package Tugas;
+
+public class Animal {
+    public void makeNoise(){
+        System.out.println("Talk");
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,26 @@
+package Tugas;
+
+public class Dog extends Animal {
+     public void makeNoise() {
+         System.out.println("Bark");
+     }
+     public static void main(String[] args) {
+     
+     Animal animal = new Animal();
+     animal.makeNoise();
+     Dog dog = new Dog();
+     dog.makeNoise();
+     Animal animaldog = new Dog();
+     animaldog.makeNoise();
+     
+     if (animal instanceof Animal)
+             System.out.println("animal is Animal");
+     if (dog instanceof Animal)
+             System.out.println("dog is Animal");
+     if (animaldog instanceof Animal)
+             System.out.println("animaldog is Animal");
+     if (animal instanceof Dog)
+             System.out.println("animal is Dog");
+     
+     }
+}
